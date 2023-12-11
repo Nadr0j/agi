dbb781978d4b6316241478979d769a0fa655a274
Jordan Fields <74157993+Nadr0jj@users.noreply.github.com>
2023-12-10 17:17:44 -0800

    debuf

--- benchmarking/config/tps_runner_config.py
+++ benchmarking/config/tps_runner_config.py
@@ -2,5 +2,5 @@ class Config:
     config = {
         "models": ["mistral:7b-instruct-q4_1"],
         "prompt": "Please list 50 unique english words.",
-        "num_runs_per_model": 2
+        "num_runs_per_model": 1
     }

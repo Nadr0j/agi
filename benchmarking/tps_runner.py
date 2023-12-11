ff57d54ad63e1cb05c0e0acaa2cdf7725938bcad
Jordan Fields <74157993+Nadr0jj@users.noreply.github.com>
2023-12-10 16:18:24 -0800

    Fix tps_runner bug in response parsing

--- benchmarking/tps_runner.py
+++ benchmarking/tps_runner.py
@@ -45,13 +45,13 @@ class TpsRunner:
         logger.info("Parsing response")
 
         try:
-            response: Dict = json.loads(raw_response.json())
+            response: Dict = raw_response.json()
             tokens_per_second = response["eval_count"] / (response["eval_duration"] / 1000000000)
             logger.info(f"Run result for {model_name} - {tokens_per_second} t/s")
             self.results_by_model[model_name].append(tokens_per_second)
         except Exception as e:
             logger.error(f"Response parsing failed for response - {raw_response.text}")
-            print(e)
+            logger.error(e)
 
     def run(self):
         self.__load_config()
